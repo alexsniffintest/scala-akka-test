581b136dcdf2dc026803f8636c6c8b91c540434c
alexsniffin <alexsniffin@gmail.com>
2019-06-16 21:05:39 -0400

    init

---
+++ src/main/scala/ScalaAkkaApi.scala
@@ -0,0 +1,34 @@
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Directives._
+import akka.stream.ActorMaterializer
+
+import scala.concurrent.ExecutionContextExecutor
+import scala.io.StdIn
+
+object ScalaAkkaApi extends App {
+
+  implicit val system: ActorSystem = ActorSystem("my-system")
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
+  implicit val executionContext: ExecutionContextExecutor = system.dispatcher
+
+  case class Test(message: String)
+
+  val route =
+    path("test") {
+      post {
+        entity(as[String]) { test =>
+          complete(test)
+        }
+      }
+    }
+
+  val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+
+  println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+  StdIn.readLine()
+  bindingFuture
+    .flatMap(_.unbind())
+    .onComplete(_ => system.terminate())
+
+}
